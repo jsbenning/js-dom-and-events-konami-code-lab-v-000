7c6d5050b2ed29df34118237e4572cc6cc3a29e0
Josef Benninghoff <jsbenning4@gmail.com>
2019-04-02 00:53:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,34 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let ind = 0;
+  let current = code[ind];
+  let len = (code.length);
+
+  function goodGuess(){
+    if (ind === len) {       
+      alert("Hurrah!  You are a genius");
+      reset();
+    } else {
+    ind++;
+    current = code[ind];
+    }
+  }
+
+  function reset(){
+    ind = 0;
+    current = code[0];
+  }
+
+
+  document.addEventListener('keydown', function(e) {
+    let myKey = parseInt(e.which || e.details);
+    if (myKey === current) {
+      goodGuess();
+    } else {
+      console.log(myKey);
+        reset();
+    }
+  });
 
 }
--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,8 @@
 describe('index', () => {
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
   function triggerKeyDown(which) {
-    const keyboardEvent = document.createEvent("KeyboardEvent")
+    const keyboardEvent = document.createEvent("KeyboardEvent");
 
     keyboardEvent.initKeyboardEvent(
       'keydown',
@@ -14,14 +14,14 @@ describe('index', () => {
       0,
       null,
       null
-    )
+    );
 
-    document.body.dispatchEvent(keyboardEvent)
+    document.body.dispatchEvent(keyboardEvent);
   }
 
   describe('Konami code', () => {
     it('triggers an alert if the right code is entered', () => {
-      init()
+      init();
 
       window.alert = expect.createSpy()
 
